fc495b269f16d41f1c170809a2931f099e319071
Udit Kumar Jain <70816866+UditJain77@users.noreply.github.com>
2022-02-07 12:56:31 +0530

    Add files via upload

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x38c3c86083D8f81828A519342F7B51CD58eb95Ac";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
